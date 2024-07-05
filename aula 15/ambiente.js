feec7fb04308726f93617bd0443124ac05868e02
Paulo Victor da Silva Carlos <paulopbv2418@gmail.com>
2024-07-04 22:59:44 -0300

    finalizei o curso de JS do cursoemvídeo

---
+++ aula 15/ambiente.js
@@ -0,0 +1,15 @@
+var num = [5,8,2,9,3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posicoes`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+var pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O valor nao foi encontrado')
+}else {
+    console.log(`O valor 8 está na posicao ${pos}`)
+}
+
+
+ 
